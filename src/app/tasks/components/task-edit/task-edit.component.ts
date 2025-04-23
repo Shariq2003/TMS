ad06d85b9bb41ed9f95fd4d6a05f89a8703d9b9e
yashi <yashi.chaudhary@acrocorp.com>
2025-04-23 18:11:46 +0530

    Edit feature Implemented

--- src/app/tasks/components/task-edit/task-edit.component.ts
+++ src/app/tasks/components/task-edit/task-edit.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Store } from '@ngxs/store';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { UpdateTask } from '../../store/actions/tasks.action';
+import { TasksState } from '../../store/states/tasks.state';
+import { Task } from '../../store/models/tasks.model';
+
 
 @Component({
   selector: 'app-task-edit',
@@ -6,5 +13,70 @@ import { Component } from '@angular/core';
   styleUrl: './task-edit.component.scss'
 })
 export class TaskEditComponent {
+
+taskForm!: FormGroup;
+taskId!: string;
+taskToEdit!: Task ;
+
+get titleControl(): FormControl {
+  return this.taskForm.get('title') as FormControl;
+}
+
+get descriptionControl(): FormControl {
+  return this.taskForm.get('description') as FormControl;
+}
+
+get statusControl(): FormControl {
+  return this.taskForm.get('status') as FormControl;
+}
+
+
+
+constructor(
+  private route: ActivatedRoute,
+  private store: Store,
+  private fb: FormBuilder,
+  private router: Router
+) {}
+
+ngOnInit(): void {
+  this.taskId = this.route.snapshot.paramMap.get('id')! ;
+  const tasks = this.store.selectSnapshot(TasksState.getTasks);
+  const task = tasks.find(t => t.id === this.taskId) || null;
+  if (!task) {
+    alert('Task not found!');
+    this.router.navigate(['/tasks']);
+return;
+}
+
+this.taskToEdit = task;
+
+this.taskForm = this.fb.group({
+  title: [task.title, Validators.required],
+  description: [task.description],
+  status: [task.status, Validators.required]
+});
+
+
+}
+
+onUpdate(): void {
+  if (this.taskForm.invalid) return;
+
+  const updatedTask: Task = {
+    ...this.taskToEdit,
+    ...this.taskForm.value
+  };
+
+  this.store.dispatch(
+    new UpdateTask({
+      id: +this.taskId,  
+      task: updatedTask
+    })
+  ).subscribe(() => {
+    this.router.navigate(['/']);
+  });
+  
+}
 
 }
